c182f0ef0c09eedf264298aa2a19a3dc81942912
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-15 15:31:56 -0400

    added js module

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('JavaScript is running!')
+let score = 100
+let email = prompt('What is your email')
+let h1 = document.createElement('h1')
+h1.innerText = 'This came from JavaScript'
---
+++ module.js
@@ -0,0 +1,14 @@
+/*
+
+Functions allow us to bundle
+
+
+*/
+
+let myName = prompt('Enter your name')
+let month = prompt('What month were you born?')
+let food = prompt('What is your favoirte food?')
+
+console.log('My name is', myName)
+console.log('I was born in', month)
+console.log('My favorite food is', food)
