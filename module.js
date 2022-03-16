8e0a7791e8a1447f619bdbe9400db16cedf9b603
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-16 13:21:06 -0400

    functions

--- module.js
+++ module.js
@@ -5,7 +5,7 @@ Functions allow us to bundle
 
 */
 
-function quiz() {
+/*function quiz() {
 let myName = prompt('Enter your name')
 let month = prompt('What month were you born?')
 let food = prompt('What is your favoirte food?')
@@ -14,3 +14,41 @@ console.log('My name is', myName)
 console.log('I was born in', month)
 console.log('My favorite food is', food)
 }
+*/
+const quiz = () => {
+    let myName = prompt('Enter your name')
+    let month = prompt('What month were you born?')
+    let food = prompt('What is your favoirte food?')
+    
+    console.log('My name is', myName)
+    console.log('I was born in', month)
+    console.log('My favorite food is', food)
+}
+
+const print = () => {
+    console.log('I love to print text')
+}
+
+const functionA = () => {
+    console.log(`FUNCTION A HAS BEEN CALLED`)
+}
+
+const functionB = () => {
+    console.log(`FUNCTION B HAS BEEN CALLED`)
+}
+const functionC = () => {
+    console.log)'FUNCTION C HAS BEEN CALLED!'
+}
+
+const greet = (person) => {
+    console.log ('Hello', person)
+}
+const blender = (string) => {
+    return string.split('')
+}
+
+const square = (num, message) => {
+    console.log('Message', message)
+    return num * num;
+}
+//arrow function are glocal while normal function is local
