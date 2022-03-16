0236125752743195da7a596c3a69adb8a9478482
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-16 17:44:11 -0400

    function done

--- rote.js
+++ rote.js
@@ -1,17 +1,25 @@
-const greet = () => {
-    console.log('Hello World')
+const multiply = (n1, n2) => {
+  console.log("Multiplying", n1,"by", n2) 
+    return n1 * n2
 }
 
-greet()
-
-const square = (num) => {
-    return num * num 
+const double = (n) => {
+    return n * 2
 }
 
-square(4)
-
-const greetByName = (str) => {
-    console.log('Hello', str)
+let array1 = ['a', 'b', 'c', 'd', 'e']
+let array2 = [1, 2, 3, 4, 5, 6, 7]
+const determineLength = (arr) => {
+    if (arr.determineLength) {
+        return 'This array is long'
+    } else {
+        return 'This array is short'
+    }
 }
 
-greetByName('Emmanuel')
+const askForString = () => {
+    let str = prompt('ENTER STRING')
+    return str
+}
+
+console.log(askForString().split(''))
