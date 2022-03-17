06a0264b1aa2e355f9eae3860389eb67bd076365
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-17 12:02:35 -0400

    added apartments

---
+++ apartments/index.js
@@ -0,0 +1,34 @@
+let apt1 = {
+    bedrooms: 2, 
+    sqft: 50, 
+    bathroom: 3, 
+    windows: 0,
+    rent: 4500,
+    unit: '1A'}
+    
+let apt2 = {bedrooms: 2, 
+    sqft: 500, 
+    bathroom: 3, 
+    windows: 0,
+    rent: 1500,
+    unit: '1B'
+}
+
+let apt3 = {
+    bedrooms: 2, 
+    sqft: 53, 
+    bathroom: 3, 
+    windows: 2,
+    rent: 2400,
+    unit: '1C'
+}
+
+
+let building = {
+    streetAddress: '74 N 7th street, booklyn 11249',
+    laundry: false,
+    allowsPets: false,
+    apartments: [apt1, apt2, apt3]
+}
+
+//builing.apartments.forEach()
