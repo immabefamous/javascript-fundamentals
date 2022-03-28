cb692be50086548fea47b8cf609e1d57c5a871ba
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-28 10:31:34 -0400

    updating

--- api.js
+++ api.js
@@ -1,35 +1,26 @@
+
 let div = document.getElementById('container')
-let data;
 
-const request = () => {
-fetch('https://raw.githubusercontent.com/immabefamous/javascript-fundamentals/main/user.json')
-.then((res) => {return res.json()})
-.then((data) => {console.log('Data returned is', data)})
+const request = async () => {
+    let req = await fetch('https://raw.githubusercontent.com/rmdashrfv/javascript-fundamentals/main/mock_data.json')
+    let res = await req.json()
+    console.log('Response', res)
+    res.forEach((element) => {
+        console.log(element)
+        let img = document.createElement('img')
+        let button = document.createElement('button')
+        let h3 = document.createElement('h3')
+        let h4 = document.createElement('h4')
+        h3.innerText = element.company_name
+        h4.innerText = `Vacancies: ${element.vacancies}`
+        button.innerText = `Rent Building ${element.id}`
+        button.addEventListener('click', () => {
+            let updatedVacancies = --element.vacancies
+            if (updatedVacancies <=-1) return alert('NO MORE VACANCIES');
+            h4.innerText = `Vacancies: ${updatedVacancies}`
+        })
+        img.setAttribute('src', element.photo)
+        div.append(h3, h4, img, button)
+    })
 }
-// Async await
-
-const request2 = () => {
-let x = fetch('https://raw.githubusercontent.com/ Endashrfv/Javascript-fundamentals/main/user.-json')
-.then((res) => {
-return res.json()
-})
-.then((data) => {
-div.innerText = data.user.email;
-return data.user.email
-})
-return 'done with function'
-}
-
-
-// Async Await
-const request3 = async () => {
-let req = await fetch('https://raw.githubusercontent.com/Imdashrfv/javascript-fundamentals/main/user.json')
-let res = await req.json()
-console.log("RESPONSE 2", res)
-data = res
-return res
-}
-
-const add = (n1, n2) => {
-n1 + n2
-}
+request()
